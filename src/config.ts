c0fc0092d4003d7b476f06964d587aa2bd2224d8
ThunderBlaze <darkthunderstormshiva@gmail.com>
2025-08-24 23:28:46 +0530

    Added Images

--- src/config.ts
+++ src/config.ts
@@ -12,7 +12,7 @@ export const siteConfig: SiteConfig = {
 	subtitle: "Blog Site",
 	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
-		hue: 270, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: true, // Hide the theme color picker for visitors
 	},
 	banner: {
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/avatar.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "ThunderBlaze",
 	bio: "Just another Anime watching CS student.",
 	links: [
